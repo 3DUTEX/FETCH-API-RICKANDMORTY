27fcc3ebd6fa8f1e14b407072d44f04239b5f1c2
3DUTEX <132105117+3DUTEX@users.noreply.github.com>
2023-07-03 04:09:44 -0300

    first commit
    API consumida, design feito e responsividade mobile feita

---
+++ main.js
@@ -0,0 +1,58 @@
+const url = "https://rickandmortyapi.com/api/character";
+
+//cria elemento html
+function create(elemento) {
+  return document.createElement(elemento);
+}
+
+//inclui elemento
+function include(onde, oque) {
+  return onde.appendChild(oque);
+}
+
+async function getAllCharacters() {
+  const response = await fetch(url);
+
+  console.log(response);
+
+  const data = await response.json();
+
+  console.log(data);
+
+  data.results.map((character) => {
+    const div = create("div");
+    const divDesc = create("div");
+    const img = create("img");
+    const name = create("h1");
+    const specie = create("p");
+    const status = create("p");
+    const location = create("h2");
+
+    div.classList.add("character");
+    divDesc.classList.add("characterDesc");
+
+    img.src = character.image;
+    name.innerText = character.name;
+    specie.innerText = character.species;
+    status.innerText = character.status;
+    location.innerText = `Local: ${character.location.name}`;
+
+    include(div, img);
+    include(div, divDesc);
+    include(divDesc, name);
+    include(divDesc, specie);
+    include(divDesc, status);
+    include(divDesc, location);
+
+    if (character.status === "Alive") {
+      status.style.color = "chartreuse";
+    } else if (character.status === "unknown") {
+      status.style.color = "#fff";
+    } else {
+      status.style.color = "red";
+    }
+    document.querySelector("#characters-container").appendChild(div);
+  });
+}
+
+getAllCharacters();
